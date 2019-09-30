a5e1dc82f241d5b84c4b78721e57daafd115ddc4
NormanLiana <liananorman@pursuit.org>
2019-09-30 16:09:17 -0400

    adds custom TV Cell

---
+++ Persist-Lab/Views/PhotosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosTableViewCell.swift
+//  Persist-Lab
+//
+//  Created by Liana Norman on 9/30/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

e8918fd6fa26671739872766fae5d4b2dd2d6d3b
NormanLiana <liananorman@pursuit.org>
2019-10-01 14:20:47 -0400

    adds one image view and 3 labels to DVC on storyboard, creates cocoa file for DVC

---
+++ Persist-Lab/Controllers/PhotosDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosDetailViewController.swift
+//  Persist-Lab
+//
+//  Created by Liana Norman on 10/1/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class PhotosDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
